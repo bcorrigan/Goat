98fb5b463ad0847484ec4837c8158992b50535d6
bcorrigan <b.j.corrigan@gmail.com>
2004-06-04 17:16:32 +0000

    encapsulated channels with getters and setters, synchronised.

--- src/goat/core/BotStats.java
+++ src/goat/core/BotStats.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * <p>Contains lots of info about the bot and its environment.
  * @version <p>Date: 18-Dec-2003</p>
- * @author <p><b>© Barry Corrigan</b> All Rights Reserved.</p>
+ * @author <p><b>? Barry Corrigan</b> All Rights Reserved.</p>
  *
  */
 public class BotStats {
@@ -16,7 +16,7 @@ public class BotStats {
 	/**
 	 * The channels we are in.
 	 */
-    public static ArrayList channels = new ArrayList();
+    private static ArrayList channels = new ArrayList();
 	/**
 	 * The authenticated owner of the bot.
 	 */
@@ -31,4 +31,33 @@ public class BotStats {
 	public static String servername;
 	public static String clientName;
 
+	public static synchronized String[] getChannels() {
+		Object[] ob = channels.toArray();
+		String[] chans = new String[ob.length];
+		for(int i=0;i<ob.length;i++) {
+			chans[i] = (String) ob[i];
+		}
+		return chans;
+	}
+
+	public static synchronized void addChannel(String chan) {
+		channels.add(chan);
+	}
+
+	public static synchronized void removeChannel(String chan) {
+		channels.remove(chan);
+	}
+
+	public static synchronized boolean containsChannel(String chan) {
+		if(channels.contains(chan))
+			return true;
+		return false;
+	}
+
+	public static boolean isValidChannelName(String chan) {
+		if((chan.startsWith("#")||chan.startsWith("+")||chan.startsWith("&"))&&!(chan.matches(":")||chan.matches(",")||chan.matches("\u0007")||chan.matches(" "))) {
+			return true;
+		}
+		return false;
+	}
 }
