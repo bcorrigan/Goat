f7bc5a465d4e0106187acc9ed7717d5ec332ec80
bcorrigan <b.j.corrigan@gmail.com>
2004-06-04 18:07:14 +0000

    Finally implemented this. Was skeletal before. Do "/msg goat say #jism hi there!" and this simple module will take care of it..

--- src/goat/module/Say.java
+++ src/goat/module/Say.java
@@ -4,16 +4,28 @@ import goat.core.Module;
 import goat.core.Message;
 
 /**
- * <p><b>© Barry Corrigan</b> All Rights Reserved.</p>
+ * <p><b>? Barry Corrigan</b> All Rights Reserved.</p>
  */
 public class Say extends Module {
 	public void processPrivateMessage(Message m) {
-		String channel = m.modCommand;
-		
-
+		if(m.isAuthorised) {
+			String[] words = m.modTrailing.split(" ");
+			String trailing="";
+			for(int i=1;i<words.length;i++)
+				trailing+=words[i] + " ";
+			new Message("", "PRIVMSG", words[0], trailing).send();
+		}
 	}
 
 	public void processChannelMessage(Message m) {
 
+	}
+
+	public int messageType() {
+		return WANT_COMMAND_MESSAGES;
+	}
+
+	public String[] getCommands() {
+		return new String[]{"say"};
 	}
 }
