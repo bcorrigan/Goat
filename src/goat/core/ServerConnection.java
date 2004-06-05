3e35d90c6689eb6b09d8681846785c246cd7c2b0
bcorrigan <b.j.corrigan@gmail.com>
2004-06-05 01:21:43 +0000

    Just stopped it printing out log crap

--- src/goat/core/ServerConnection.java
+++ src/goat/core/ServerConnection.java
@@ -96,8 +96,8 @@ public class ServerConnection extends Thread {
 						if(m.command.equals("PING"))
 							outqueue.enqueue(new Message("", "PONG", "", m.trailing));
                         else inqueue.enqueue(m); //add to inqueue
-						System.out.println("Inbuffer: prefix: " + m.prefix + " params: " + m.params + " trailing:" + m.trailing + " command:" + m.command + " sender: " + m.sender +
-								           "\n    " + "isCTCP:" + m.isCTCP + " isPrivate:" + m.isPrivate + " CTCPCommand:" + m.CTCPCommand + " CTCPMessage:" + m.CTCPMessage);
+						//System.out.println("Inbuffer: prefix: " + m.prefix + " params: " + m.params + " trailing:" + m.trailing + " command:" + m.command + " sender: " + m.sender +
+						//		           "\n    " + "isCTCP:" + m.isCTCP + " isPrivate:" + m.isPrivate + " CTCPCommand:" + m.CTCPCommand + " CTCPMessage:" + m.CTCPMessage);
                     } else {
 						if((System.currentTimeMillis()-lastActivity)>305000) {
 							in.close();
