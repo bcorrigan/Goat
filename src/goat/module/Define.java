e15e5d55fadfb203cf2c45d8c53074c847925a45
robotslave <robotslave@gmail.com>
2004-08-20 22:59:18 +0000

    main() not working yet, don't know why.

---
+++ src/goat/module/Define.java
@@ -0,0 +1,64 @@
+package goat.module;
+
+import goat.core.Module;
+import goat.core.Message;
+import goat.core.BotStats;
+
+import java.io.*;
+//import java.util.ArrayList ;
+//import java.util.Iterator ;
+import java.util.Random ;
+import net.zuckerfrei.jcfd.* ;
+
+/**
+ * Copyright (c) 2004 Robot Slave Enterprise Solutions
+ * 
+ * @title Define
+ * 
+ *	@author encontrado
+ * 
+ * @version 1.0
+ */
+
+public class Define extends Module {
+
+	private static Dict dict ;
+	
+	public int messageType() {
+		return WANT_ALL_MESSAGES;
+	}
+   public String[] getCommands() {
+		return new String[]{"define", "randef" };
+   }
+	
+	public Define() {
+		try {
+			dict = DictFactory.getInstance().getDictClient() ;
+		} catch (DictException e) {
+			System.out.println("Dict Exeption:") ;
+			System.exit(1) ;
+		}
+	}
+
+	public void processPrivateMessage(Message m) {
+		processChannelMessage(m) ;
+	}
+
+	public void processChannelMessage(Message m) {
+		//parse out args
+		if (m.modCommand.equals("define")) {
+			//blither blither
+			m.createReply("blather").send() ;
+		} else if (m.getWord(0).equals("randef")) {
+		}
+	}
+
+	public static void main(String[] arg) {
+		DatabaseList dbList = dict.listDatabases() ;
+		Database db ;
+		while (dbList.hasNext()) {
+			db = dbList.next() ;
+			System.out.println(db.getCode() + " : " + db.getName() + " : " + db.toString()) ;
+		}
+	}
+}
