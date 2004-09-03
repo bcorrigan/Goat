0f9580150549b0bbf9b297cf35df616228f3b710
robotslave <robotslave@gmail.com>
2004-09-03 01:42:30 +0000

    rejiggered for new, far saner DICT client library.

--- src/goat/module/Define.java
+++ src/goat/module/Define.java
@@ -2,32 +2,23 @@ package goat.module ;
 
 import goat.core.Module;
 import goat.core.Message;
-import goat.core.BotStats;
+import goat.util.DICTClient;
 
 import java.io.*;
-//import java.util.ArrayList ;
-//import java.util.Iterator ;
+import java.net.*;
 import java.util.Random ;
-import net.zuckerfrei.jcfd.DictFactory ;
-import net.zuckerfrei.jcfd.Dict ;
-import net.zuckerfrei.jcfd.DictException ;
-import net.zuckerfrei.jcfd.DatabaseList ;
-import net.zuckerfrei.jcfd.Database ;
+
 
 /**
  * Copyright (c) 2004 Robot Slave Enterprise Solutions
- * 
- * @title Definer
  * 
  *	@author encontrado
  * 
  * @version 1.0
  */
-
 public class Define extends Module {
-
-	private static Dict dict ;
 	
+	private static DICTClient dictClient = null ;
 	public int messageType() {
 		return WANT_COMMAND_MESSAGES;
 	}
@@ -35,27 +26,32 @@ public class Define extends Module {
 		return new String[]{"define", "randef", "dictionaries", "dictionary" };
    }
 	
-	public void Define() {
-		init() ;
+	public Define() {
+		if ( null == dictClient ) 
+			init() ;
 	}
-	
+
 	private void init() {
 		try {
-			DictFactory df = DictFactory.getInstance() ;
-			dict = df.getDictClient() ;
-		} catch (DictException e) {
-			System.out.println("Dict Exception:") ;
+			dictClient = new DICTClient("www.edslocomb.com") ;
+		} catch (UnknownHostException e) {
+			e.printStackTrace() ;
+			System.exit(1) ;
+		} catch (ConnectException e) {
 			e.printStackTrace() ;
 			System.exit(1) ;
 		}
 	}
-
+		
+	
 	public void processPrivateMessage(Message m) {
 		processChannelMessage(m) ;
 	}
 
 	public void processChannelMessage(Message m) {
 		//parse out args
+
+		System.out.println("processing command: " + m.modCommand) ;
 		if (m.modCommand.equals("define")) {
 			define(m) ;
 		} else if (m.modCommand.equals("randef")) { 
@@ -76,39 +72,47 @@ public class Define extends Module {
 	}
 	
 	private void dictionaries(Message m) {
-		DatabaseList dbList = dict.listDatabases() ;
-		Database db ;
+		String[][] dbList = dictClient.getDatabases() ;
 		String line = "" ;
-		while (dbList.hasNext()) {
-			db = dbList.next() ;
-			line = line + ", " + db.getCode() ;
+		for (int i = 0; i < dbList.length ; i++) {
+			if (line.equals(""))
+				line = dbList[i][0] ;
+			else
+				line = line + ", " + dbList[i][0] ;
+			line = line.trim() ;
 		}
-		line = line.trim() ;
 		m.createPagedReply(line).send() ;
 	}
 	
 	private void dictionary(Message m) {
-		m.createReply("Not implmemented, please stand by").send() ; 
+		String code = m.modTrailing.trim() ;
+		String line = "" ;
+		String[][] dbList = dictClient.getDatabases() ;
+		boolean found = false ;
+		for (int i = 0; i < dbList.length ; i++ ) {
+			if (dbList[i][0].equals(code)) {
+				found = true ;
+				line = code + ": " + dbList[i][1] ;
+				break ;
+			}
+			if (line.equals(""))
+				line = dbList[i][0] ;
+			else
+				line = line + ", " + dbList[i][0] ;
+			line = line.trim() ;
+		}
+		if (! found) 
+			line = "Dictionary \"" + code + "\" not found; available dictionaries: " + line ;
+		m.createPagedReply(line).send() ;
 	}
 
 	public static void main(String[] arg) {
+		new Define() ;
 		System.out.println("Starting main()") ;
-		
-		Define d = new Define() ;
-		d.Define() ; // Seems the constructor isn't actually called in a static context, or some such ballocks, so we need this.
-		System.out.println("initialized.") ;
-		if (null == dict) {
-			System.out.println("null dict, dying") ;
-			System.exit(1) ;
-		}
-
-		DatabaseList dbList ;
-		dbList = dict.listDatabases() ;
-		Database db ;
+		String[][] dbList = dictClient.getDatabases() ;
 		String line = "" ;
-		while (dbList.hasNext()) {
-			db = dbList.next() ;
-			line = line + " " + db.getCode() ;
+		for (int i = 0 ; i < dbList.length ; i++) {
+			line = line + " " + dbList[i][0] ;
 		}
 		line = line.trim() ;
 		System.out.println(line) ;
