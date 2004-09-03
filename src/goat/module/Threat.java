eb3f9f4b995b87d504cf7c0b7e1bf73f97b2e691
bcorrigan <b.j.corrigan@gmail.com>
2004-09-03 20:21:05 +0000

    Fixed the name of the DHS (Homeland, not Home) and added a TODO item. Be nice when metadata for todos comes in 1.5.

--- src/goat/module/Threat.java
+++ src/goat/module/Threat.java
@@ -17,58 +17,58 @@ import java.io.InputStreamReader;
  * <p/>
  * The levels are:
  * <p/>
- * <ul>
- * <li>Green (low danger)
- * <li>Blue (be guarded)
- * <li>Yellow (elevated)
- * <li>Orange (high)
- * <li>Red (shit jeeesus I'm coming home!)
- * </ul>
+ * <ul> <li>Green (low danger) <li>Blue (be guarded) <li>Yellow (elevated) <li>Orange (high) <li>Red (shit jeeesus I'm
+ * coming home!) </ul>
+ *
  * @author Barry Corrigan
  */
 public class Threat extends Module implements Runnable {
 
-	private static final int UNKNOWN=0;
-	private static final int GREEN=1;
-	private static final int BLUE=2;
-	private static final int YELLOW=3;
-	private static final int ORANGE=4;
-	private static final int RED=5;
+	private static final int UNKNOWN = 0;
+	private static final int GREEN = 1;
+	private static final int BLUE = 2;
+	private static final int YELLOW = 3;
+	private static final int ORANGE = 4;
+	private static final int RED = 5;
 
 	private static final String[] THREATS = new String[6];
+
 	static {
-		THREATS[UNKNOWN]="UNKNOWN";
+		THREATS[UNKNOWN] = "UNKNOWN";
 		THREATS[GREEN] = Message.GREEN + "GREEN" + Message.NORMAL + " (low danger)";
 		THREATS[BLUE] = Message.BLUE + "BLUE" + Message.NORMAL + " (be guarded)";
 		THREATS[YELLOW] = Message.YELLOW + "YELLOW" + Message.NORMAL + " (elevated danger level)";
 		THREATS[ORANGE] = Message.BROWN + "ORANGE" + Message.NORMAL + " (high danger level)";
 		THREATS[RED] = Message.RED + "RED" + Message.NORMAL + " (severe danger level)";
 	}
+
 	private int threatLevel;
 
-	private boolean firstTime=true;	//true until commitThreat is run at least once, so that channels aren't spammed when module is first loaded
+	private boolean firstTime = true;	//true until commitThreat is run at least once, so that channels aren't spammed when module is first loaded
 
 	public Threat() {
 		Thread t = new Thread(this);
 		t.start();
 	}
+
 	public void processPrivateMessage(Message m) {
-    	processChannelMessage(m);
+		processChannelMessage(m);
 	}
 
 	public void processChannelMessage(Message m) {
-        m.createReply("The current Department of Home Security terror threat level is " + THREATS[threatLevel]).send();
+		m.createReply("The current Department of Homeland Security terror threat level is " + THREATS[threatLevel]).send();
 	}
 
 	public String[] getCommands() {
 		return new String[]{"threat"};
-   }
+	}
 
 	/**
-	 * A wee thread that gets the latest threat elvel every ten minutes and sends out messages informing folk of changes in the terror level.
+	 * A wee thread that gets the latest threat elvel every ten minutes and sends out messages informing folk of changes in
+	 * the terror level.
 	 */
 	public void run() {
-		while(true) {
+		while (true) {
 			commitThreat(getLatestThreatLevel());
 			try {
 				Thread.sleep(600);		//update the threat level every ten minutes
@@ -80,6 +80,7 @@ public class Threat extends Module implements Runnable {
 
 	/**
 	 * Contacts http://www.threat-advisory.com/formal.html yo get the latest threat level.
+	 *
 	 * @return The latest threat level
 	 */
 	private int getLatestThreatLevel() {
@@ -94,17 +95,17 @@ public class Threat extends Module implements Runnable {
 			}
 			BufferedReader in = new BufferedReader(new InputStreamReader(threatURL.openStream()));
 			String threatLevelString = in.readLine();
-			if(threatLevelString.equals("Green"))
+			if (threatLevelString.equals("Green"))
 				return GREEN;
-			else if(threatLevelString.equals("Blue"))
+			else if (threatLevelString.equals("Blue"))
 				return BLUE;
-			else if(threatLevelString.equals("Yellow"))
+			else if (threatLevelString.equals("Yellow"))
 				return YELLOW;
-			else if(threatLevelString.equals("Orange"))
+			else if (threatLevelString.equals("Orange"))
 				return ORANGE;
-			else if(threatLevelString.equals("Red"))
+			else if (threatLevelString.equals("Red"))
 				return RED;
-            connection.disconnect();
+			connection.disconnect();
 			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -114,47 +115,48 @@ public class Threat extends Module implements Runnable {
 
 	/**
 	 * Takes a new threat level from the source website and compares to last one, decides on any actions to be taken, etc
+	 *
 	 * @param newThreat The very latest threat level
 	 */
 	private void commitThreat(int newThreat) {
-    	if(newThreat==UNKNOWN)
+		if (newThreat == UNKNOWN)
 			return;
-		else if(firstTime==true) {
-			firstTime=false;
-			threatLevel=newThreat;
+		else if (firstTime == true) {
+			firstTime = false;
+			threatLevel = newThreat;
 			return;
-		} else if(newThreat==threatLevel)
+		} else if (newThreat == threatLevel)
 			return;
-		else if(newThreat>threatLevel) {
-			threatLevel=newThreat;
-			switch(threatLevel) {
+		else if (newThreat > threatLevel) {
+			threatLevel = newThreat;
+			switch (threatLevel) {
 				case BLUE:
-					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Home Security has upgraded the threat level to " + Message.BLUE + "Blue" + Message.NORMAL + ", this is not dangerous, but please be guarded.");
+					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Homeland Security has upgraded the threat level to " + Message.BLUE + "Blue" + Message.NORMAL + ", this is not dangerous, but please be guarded.");
 					break;
 				case YELLOW:
-					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Home Security has upgraded the threat level to " + Message.YELLOW + "Yellow" + Message.NORMAL + ", there is now a significant risk of terrorist attacks. Please take appropriate measures to safeguard yourself and your loved ones.");
+					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Homeland Security has upgraded the threat level to " + Message.YELLOW + "Yellow" + Message.NORMAL + ", there is now a significant risk of terrorist attacks. Please take appropriate measures to safeguard yourself and your loved ones.");
 					break;
 				case ORANGE:
-					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Home Security has upgraded the threat level to " + Message.BROWN + "Orange" + Message.NORMAL + ", there is now a " + Message.BOLD + "very high" + Message.BOLD + " risk of terrorist attack! Please stay away from urban areas and consider stockpiling essential supplies.");
-				    break;
+					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Homeland Security has upgraded the threat level to " + Message.BROWN + "Orange" + Message.NORMAL + ", there is now a " + Message.BOLD + "very high" + Message.BOLD + " risk of terrorist attack! Please stay away from urban areas and consider stockpiling essential supplies.");
+					break;
 				case RED:
-					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Home Security has upgraded the threat level to " + Message.RED + "Red" + Message.NORMAL + ", the risk of terrorist attack is now " + Message.BOLD + "severe" + Message.BOLD + ", you should leave the country if possible (not by plane) and convert your assets into gold.");
+					broadcast(Message.BOLD + "Caution: " + Message.BOLD + "The Department of Homeland Security has upgraded the threat level to " + Message.RED + "Red" + Message.NORMAL + ", the risk of terrorist attack is now " + Message.BOLD + "severe" + Message.BOLD + ", you should leave the country if possible (not by plane) and convert your assets into gold.");
 			}
-		} else if(newThreat<threatLevel) {
-			threatLevel=newThreat;
-			switch(threatLevel) {
+		} else if (newThreat < threatLevel) {
+			threatLevel = newThreat;
+			switch (threatLevel) {
 				case GREEN:
-					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Home Security has downgraded the threat level to " + Message.GREEN + "Green" + Message.NORMAL + ", the risk of terrorist attack is low, but don't take anything for granted.");
+					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Homeland Security has downgraded the threat level to " + Message.GREEN + "Green" + Message.NORMAL + ", the risk of terrorist attack is low, but don't take anything for granted.");
 					break;
 				case BLUE:
-					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Home Security has downgraded the threat level to " + Message.BLUE + "Blue" + Message.NORMAL + ", this is still dangerous, so please be guarded.");
+					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Homeland Security has downgraded the threat level to " + Message.BLUE + "Blue" + Message.NORMAL + ", this is still dangerous, so please be guarded.");
 					break;
 				case YELLOW:
-					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Home Security has downgraded the threat level to " + Message.YELLOW + "Yellow" + Message.NORMAL + ", there is still a significant risk of terrorist attacks. Continue take appropriate measures to safeguard yourself and your loved ones.");
+					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Homeland Security has downgraded the threat level to " + Message.YELLOW + "Yellow" + Message.NORMAL + ", there is still a significant risk of terrorist attacks. Continue take appropriate measures to safeguard yourself and your loved ones.");
 					break;
 				case ORANGE:
-					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Home Security has downgraded the threat level to " + Message.BROWN + "Orange" + Message.NORMAL + ", there is still a " + Message.BOLD + "very high" + Message.BOLD + " risk of terrorist attack! You can relax a little, but times are still grave.");
-				    break;
+					broadcast(Message.BOLD + "Update: " + Message.BOLD + "The Department of Homeland Security has downgraded the threat level to " + Message.BROWN + "Orange" + Message.NORMAL + ", there is still a " + Message.BOLD + "very high" + Message.BOLD + " risk of terrorist attack! You can relax a little, but times are still grave.");
+					break;
 
 			}
 		}
@@ -162,11 +164,12 @@ public class Threat extends Module implements Runnable {
 
 	/**
 	 * Pump out message to all channels goat is on.
+	 *
 	 * @param msg The message to get sent.
 	 */
-	private void broadcast(String msg) {
-    	String[] chans = BotStats.getChannels();
-		for(int i=0;i<chans.length;i++)
+	private void broadcast(String msg) {         //TODO: This should really only broadcast to the channels the module is on, that means an accessor and some modifications through goat.core.* though
+		String[] chans = BotStats.getChannels();
+		for (int i = 0; i < chans.length; i++)
 			new Message("", "PRIVMSG", chans[i], msg).send();
 	}
 }
