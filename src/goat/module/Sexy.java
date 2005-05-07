714a963378bb1004ed3c921afeda5e62b148d952
robotslave <robotslave@gmail.com>
2005-05-07 04:47:36 +0000

    Oops.  Didn't use the right matchstring when reverting from String.contains() to string.match(); fixed.

--- src/goat/module/Sexy.java
+++ src/goat/module/Sexy.java
@@ -41,8 +41,9 @@ public class Sexy extends Module {
     private long getNumResults(String searchString) throws IOException {
         BufferedReader in = null;
         HttpURLConnection connection = null;
+		  String google = "www.google.com" ;
         try {
-            URL url = new URL("http://www.google.co.uk/search?q=" + searchString.replaceAll(" ", "+"));
+            URL url = new URL("http://" + google + "/search?q=" + searchString.replaceAll(" ", "+"));
             connection = (HttpURLConnection) url.openConnection();
             // incompatible with 1.4
             // connection.setConnectTimeout(3000);  //just three seconds, we can't hang around
@@ -55,27 +56,46 @@ public class Sexy extends Module {
             connection.connect();
 
             if (connection.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
-                throw new IOException("Whoa, google gives an HTTP_NOT_FOUND");
+                throw new IOException("Whoa, " + google + " gives an HTTP_NOT_FOUND");
             }
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException("Hmmmn, the server is giving me an HTTP Status-Code " + connection.getResponseCode() + ", sorry.");
-            }
+                throw new IOException("Hmmmn, " + google + " is giving me an HTTP Status-Code " + connection.getResponseCode() + ", sorry.");
+            } else {
+					// Debuggo
+					//System.out.println("Sexy: connection OK") ;
+				}
             in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				// Debug block
+				in.mark(2048) ;
+				if (null == in.readLine()) {
+					System.out.println("Sexy: Recieved null content from " + google) ;
+				} else {
+					in.reset() ;
+				}
+				
+				// Parse page
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                if (inputLine.matches("color=>Results")) { //in the content line
+                if (inputLine.matches(".*color=>Results.*")) { //in the content line
+						  // Debuggo 
+						  // System.out.println("Sexy: parsing line: " + inputLine) ;
                     inputLine = inputLine.substring(0,477); //ignore later shite it can confuse and varies too much
                     inputLine = inputLine.replaceAll(".*</b> of about <b>", ""); //zap everything before
                     inputLine = inputLine.replaceAll(".*\\d</b> of <b>", ""); //sometimes above not work when just 1 result
                     inputLine = inputLine.replaceAll("<.*", "");        //zap everything after
                     inputLine = inputLine.replaceAll(",", "");         //get rid of commas, should just be left with a number now
+						  // Debuggo
+						  // System.out.println("Sexy: left with: " + inputLine) ;
                     try {
                         return Long.parseLong(inputLine);
                     } catch (NumberFormatException nfe) {
                         nfe.printStackTrace();
-                        throw new IOException("omg a parse error: " + nfe.getMessage());
+                        throw new IOException("Sexy: omg a parse error: " + nfe.getMessage());
                     }
-                }
+                } else {
+						 // Mondo Debuggo
+						 // System.out.println("Sexy: " + inputLine) ;
+					 }
             }
         } finally {
             if (in != null) in.close();
