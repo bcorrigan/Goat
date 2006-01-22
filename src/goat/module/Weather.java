e21715c78e2ab14bc80640d5ff17ddaf2466e860
robotslave <robotslave@gmail.com>
2006-01-22 21:10:27 +0000

    Added sunrise/sunset times to weather report

--- src/goat/module/Weather.java
+++ src/goat/module/Weather.java
@@ -21,6 +21,9 @@ import java.net.HttpURLConnection;
 import java.util.regex.* ;
 import java.util.Date ;
 import java.text.SimpleDateFormat ;
+import com.web_tomorrow.utils.suntimes.*;
+import java.util.GregorianCalendar ;
+import java.util.TimeZone ;
 
 /**
  * @author bc
@@ -116,7 +119,8 @@ public class Weather extends Module {
 				return "Hmmmn, " + user.getName() + ", the NOAA weather server is giving me an HTTP Status-Code " + connection.getResponseCode() + ", sorry.";
 			}
 			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String inputLine, response = in.readLine() + ' ';
+			String inputLine = null ; 
+			String response = "" ;
 			String wind_direction = "";
 			String wind_mph = "";
 			String wind_gust = "";
@@ -126,6 +130,12 @@ public class Weather extends Module {
 			String precipitation = "none";
 			String humidity = "";
 			long minutes_since_report = 0 ;
+			String report_timezone = "" ;
+			int report_year = 0;
+			int report_month = 0;
+			int report_day = 0;
+			int longitude = 0;
+			int latitude = 0;
 			while ((inputLine = in.readLine()) != null) {
 				if (inputLine.startsWith("ob") || inputLine.startsWith("cycle"))
 					continue;
@@ -136,7 +146,9 @@ public class Weather extends Module {
 				}
 				response += inputLine + ' ';
 				
-				// Might want to move these pattern compiles out of the while loop
+				// Might want to move these pattern compiles out of the while loop...
+
+				// Wind
 				Matcher m = Pattern.compile("^Wind: from the ([NSEW]+) \\(.*\\) at (\\d+) MPH \\(\\d+ KT\\)(?: gusting to (\\d+) MPH \\(\\d+ KT\\))*.*").matcher(inputLine) ;
 				if (m.matches()) {
 					wind_direction = m.group(1) ;
@@ -145,16 +157,44 @@ public class Weather extends Module {
 						wind_gust = m.group(3) ;
 					}
 				}
+
+				// Coordinates
+				m = Pattern.compile(".* (\\d+)-\\d+(?:-\\d+)*([NS]) (\\d+)-\\d+(?:-\\d+)*([EW]).*").matcher(inputLine) ;
+				if (m.matches()) {
+					System.out.println("matched: " + m.group()) ;
+					latitude = Integer.parseInt(m.group(1)) ;
+					if ((m.group(2) != null) && (m.group(2).equals("S"))) {
+						latitude = - latitude ;
+					}
+					longitude = Integer.parseInt(m.group(3)) ;
+					if ((m.group(4) != null) && (m.group(4).equals("W"))) {
+						longitude = - longitude ;
+					}
+				}
+
+				// Sky conditions
 				m = Pattern.compile("^Sky conditions: (.*)").matcher(inputLine) ;
 				if (m.matches()) {
 					sky_conditions = m.group(1) ;
 				}
-				m = Pattern.compile(".*/ (\\d+\\.\\d+\\.\\d+ \\d+ UTC).*").matcher(inputLine) ;
+
+				// Time
+				m = Pattern.compile(".* ([A-Z]{3}) / ((\\d+)\\.(\\d+)\\.(\\d+) \\d+ UTC).*").matcher(inputLine) ;
 				if (m.matches()) {
-					Date now = new Date() ;
+					// By way of explanation:  the regexp should yield groups:
+					//  (1) local time zone as "ZZZ"
+					report_timezone = m.group(1) ;
+					//  (2) UTC date and time as "yyyy.MM.dd HHmm UTC"
+					//  (3) year as "yyyy"
+					report_year = Integer.parseInt(m.group(3)) ;
+					//  (4) month as "MM"
+					report_month = Integer.parseInt(m.group(4)) ;
+					//  (5) day as "dd"
+					report_day = Integer.parseInt(m.group(5)) ;
 					SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HHmm zzz") ;
 					try { 
-						long diff = now.getTime() - sdf.parse(m.group(1)).getTime() ;
+						Date now = new Date() ;
+						long diff = now.getTime() - sdf.parse(m.group(2)).getTime() ;
 						minutes_since_report = diff / (1000L*60L) ;
 					} catch(java.text.ParseException e) {
 						System.out.println("Date parse exception.") ;
@@ -162,20 +202,38 @@ public class Weather extends Module {
 					}
 						
 				}
+
+				// Temperature
 				m = Pattern.compile("^Temperature: ([\\d.-]+) F \\(([\\d.-]+) C\\)").matcher(inputLine) ;
 				if (m.matches()) {
 					temp_f = m.group(1) ;
 					temp_c = m.group(2) ;
 				}
+
+				// Precipitation
 				m = Pattern.compile("^Precipitation last hour: (.*)").matcher(inputLine) ;
 				if (m.matches()) {
 					precipitation = m.group(1) ;
 				}
+				// Humidity
 				m = Pattern.compile("^Relative Humidity: (.*)").matcher(inputLine) ;
 				if (m.matches()) {
 					humidity = m.group(1) ;
 				}
 			}
+			String sunrise_string = "" ;
+			String sunset_string = "" ;
+			//  Note: crappily named class Time is bundled in with the suntimes lib
+			Time sunrise_UTC = new Time(0) ;
+			Time sunset_UTC = new Time(0) ;
+			try {
+				sunrise_UTC = SunTimes.getSunriseTimeUTC(report_year, report_month, report_day, longitude, latitude, SunTimes.ZENITH) ;
+				sunset_UTC = SunTimes.getSunsetTimeUTC(report_year, report_month, report_day, longitude, latitude, SunTimes.ZENITH) ;
+			} catch (SunTimesException e) {
+				e.printStackTrace() ;
+			}
+			sunrise_string = sunString(sunrise_UTC, "sunrise", longitude, null) ;
+			sunset_string = sunString(sunset_UTC, "sunset", longitude, null) ;
 			String short_response = temp_f + "F/" + temp_c + "C, " 
 				+ sky_conditions + ".  " + "Wind " + wind_direction + " " 
 				+ wind_mph + "mph" ;
@@ -185,6 +243,10 @@ public class Weather extends Module {
 			short_response += ".  Humidity " + humidity ;
 			if (! precipitation.equals("none")) {
 				short_response += ".  Precipitation: " + precipitation ;
+			}
+			if (! (sunrise_string.equals("") || sunset_string.equals(""))) {
+				short_response += ".  " + sunrise_string + ", " + sunset_string ;
+				response += " " + sunrise_string + ", " + sunset_string ;
 			}
 			if (0 != minutes_since_report) {
 				short_response += ".  Reported " + minutes_since_report + " minutes ago at " + station + "." ;
@@ -219,6 +281,42 @@ public class Weather extends Module {
             if(XMLenc!=null) XMLenc.close();
         }
 	}
+
+	private String sunString(Time t, String type, int longitude, TimeZone tz) {
+		if (type.equalsIgnoreCase("sunrise")) {
+			type = "Sunrise" ;
+		} else if (type.equalsIgnoreCase("sunset")) {
+			type = "Sunset" ;
+		} else {
+			return "" ;
+		}
+		String ret = type + ": " ;
+		GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("UTC")) ;
+		cal.set(cal.HOUR_OF_DAY, t.getHour()) ;
+		cal.set(cal.MINUTE, t.getMinute()) ;
+		if (null == tz) {
+			// Fake time zone conversion
+			cal.add(cal.HOUR_OF_DAY, longitude / 15) ;
+		} else {
+ 	   	// Real time zone conversion
+			long tempdate = cal.getTimeInMillis() ;
+			cal = new GregorianCalendar(tz) ;
+			cal.setTimeInMillis(tempdate) ;
+		}
+		ret = ret + cal.get(cal.HOUR) + ":" ;
+		if (10 > cal.get(cal.MINUTE)) {
+			ret = ret + "0" + cal.get(cal.MINUTE) ;
+		} else {
+			ret = ret + cal.get(cal.MINUTE) ;
+		}
+		if ( cal.get(cal.AM_PM) == cal.AM ) {
+			ret += "am" ;
+		} else {
+			ret += "pm" ;
+		}
+		return ret ;
+	}
+		
 
 	public String[] getCommands() {
 		return new String[]{"weather", "fullweather"};
