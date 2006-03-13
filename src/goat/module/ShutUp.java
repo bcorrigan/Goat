48752b6274f744f9984f3118356927ea9e736175
robotslave <robotslave@gmail.com>
2006-03-13 22:37:57 +0000

    Long overdue module, quick hack.

---
+++ src/goat/module/ShutUp.java
@@ -0,0 +1,56 @@
+package goat.module;
+
+import goat.core.Message;
+import goat.core.Module;
+import java.util.*;
+
+
+/**
+ * Module to tell people to shut up.
+ * 
+ * @author rs
+ *
+ */
+public class ShutUp extends Module {
+
+	
+	public int messageType() {
+		return WANT_ALL_MESSAGES ;
+	}
+	
+	@Override
+	public void processPrivateMessage(Message m) {
+		processChannelMessage(m) ;
+	}
+
+	@Override
+	public void processChannelMessage(Message m) {
+		// TODO make this general, right now it's just a quick hack to fill a pressing need.
+		
+		ArrayList<String> joeyisms = new ArrayList() ;
+		joeyisms.add("dogs are fat") ;
+		joeyisms.add("proliferate") ;
+		joeyisms.add("huge boobs") ;
+		if (fires(m.trailing, joeyisms)) {
+			m.createReply("Shut up, joey.").send() ;
+		}
+	}
+	
+	/**
+	 * check to see if any string in a list in a list of trigger strings is
+	 * contained in the target string.
+	 * 
+	 * @param m
+	 * @param phrases
+	 * @return
+	 */
+	private boolean fires(String target, ArrayList<String> triggers) {
+		Iterator<String> i = triggers.iterator() ;
+		String s ;
+		while ((s = i.next()) != null) {
+			if(target.toLowerCase().contains(s.toLowerCase()))
+				return true ;
+		}
+		return false ;
+	}
+}
