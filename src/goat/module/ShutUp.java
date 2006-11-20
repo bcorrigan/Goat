cd5c368b4eaaa59ce9f2d1c05c382ea00573a59d
robotslave <robotslave@gmail.com>
2006-11-20 20:11:08 +0000

    Added blather detector, simplified things a bit.

--- src/goat/module/ShutUp.java
+++ src/goat/module/ShutUp.java
@@ -2,7 +2,9 @@ package goat.module;
 
 import goat.core.Message;
 import goat.core.Module;
-import java.util.*;
+import java.util.Random;
+
+
 
 /**
  * Module to tell people to shut up.
@@ -12,7 +14,56 @@ import java.util.*;
  */
 public class ShutUp extends Module {
 
+	public static final String[] joeyisms = {
+			"dogs are fat",	
+			"proliferate", 
+			"proliferating", 
+			"proliferation", 
+			"Erica Campbell", 
+			"huge boobs", 
+			"regina", 
+			"katie", 
+			"marge", 
+			"nerd virgin", 
+			"nergin", 
+			"true pimp", 
+			"melons", 
+			"power up", 
+			"saiyan", 
+			"vegeta ",
+			"freethinking atheists", 
+			"totally gay" } ;
+	
+	public static final String[] kipisms = {
+			"bliar",
+			"wogs",
+			"labour",
+	} ;
+	
+	// ideally, these should be in order of increasing severity, and not look
+	//   odd when prefixed with "[uname], "
+	public static final String[] responses = {
+			"shut up.",
+			"shut the fuck up.",
+			"fucking shut up.",
+			"fuck off.  Now.",
+			"give it a fucking rest.",
+			"please shut the fuck up.",
+			"you're being fucking boring.",
+			"you're fucking boring us.  " + Message.BOLD + "Again.",
+			"have you forgotten to take your fucking medicine?", 
+			"get your greasy little fingers away from the fucking computer.",
+			} ;
+
 	private Random random = new Random() ;
+	
+	private String blatherer = "";     // who is blathering?
+	private int blatherCount = 0;      // how long have they been blathering?
+	private int blatherThreshold = 4 ; // how much of this blather will we put up with?
+	private int kipismCount = 0;       // and is it that bastard kip again?
+	
+	public ShutUp() {
+	}
 	
 	public int messageType() {
 		return WANT_ALL_MESSAGES ;
@@ -20,80 +71,56 @@ public class ShutUp extends Module {
 	
 	@Override
 	public void processPrivateMessage(Message m) {
-		processChannelMessage(m) ;
+		// do nothing here, people can yammer at goat in private to their heart's content.
 	}
-
+	
 	@Override
 	public void processChannelMessage(Message m) {
-		// TODO make this general, right now it's just a quick hack to fill a pressing need.
-		
-		ArrayList<String> joeyisms = new ArrayList<String>() ;
-		joeyisms.add("dogs are fat") ;
-		joeyisms.add("proliferate") ;
-		joeyisms.add("proliferating") ;
-		joeyisms.add("proliferation") ;
-		joeyisms.add("Erica Campbell") ;
-		joeyisms.add("huge boobs") ;
-		joeyisms.add("regina") ;
-		joeyisms.add("katie") ;
-		joeyisms.add("marge") ;
-		joeyisms.add("true pimp") ;
-		joeyisms.add("nerd virgin") ;
-		joeyisms.add("nergin") ;
-		joeyisms.add("melons") ;
-		joeyisms.add("power up") ;
-		joeyisms.add("saiyan") ;
-		joeyisms.add("vegeta ") ;
-		joeyisms.add("freethinking athiests");
-		joeyisms.add("totally gay");
-	
-		ArrayList<String> joeyResponses = new ArrayList<String>() ;
-		joeyResponses.add("joey, shut the fuck up.") ;
-		joeyResponses.add("joey, give it a fucking rest.") ;
-		joeyResponses.add("joey, you're getting fucking boring again.") ;
-		joeyResponses.add("joey: fuck off.  Now.") ;
-		joeyResponses.add("joey, please shut the fuck up already.") ;
-		joeyResponses.add("joey: shut up.") ;
-		joeyResponses.add("joey, get your greasy little fingers away from the fucking computer.") ;
-		joeyResponses.add("joey: fucking shut up.") ;
-		joeyResponses.add("joey, have you stopped taking your medicine again?") ;
 
-		if (fires(m.trailing, joeyisms)) {
+		// per-line joey shutting-up
+		int joeyscore = score(m.trailing, joeyisms) ;
+		if (joeyscore > 0) {
 			// debug
 			// System.out.println("JOEYISM detected from umask: " + m.prefix) ;
 			if(m.prefix.matches(".*\\.pacbell\\.net.*")) {
 				// for now, we'll only respond to joey himself occasionally, 
 				//  since he seems to be getting off on this.
-				if (random.nextInt(100) < 34 )
-					m.createReply(pickRandom(joeyResponses)).send() ;
+				if (random.nextInt(100) < (30 + 5*joeyscore) )
+					m.createReply("joey, " + pickRandom(responses)).send() ;
 				// debug
 				//System.out.println("JOEY detected!") ;
 			} else {
 				m.createReply("Shut up, joey.").send() ;
 			}
 		}
-	}
-	
-	/**
-	 * check to see if any string in a list in a list of trigger strings is
-	 * contained in the target string.
-	 * 
-	 * @param m
-	 * @param phrases
-	 * @return
-	 */
-	private boolean fires(String target, ArrayList<String> triggers) {
-		Iterator<String> i = triggers.iterator() ;
-		String s ;
-		while (i.hasNext()) {
-			s = i.next() ;
-			if(target.toLowerCase().contains(s.toLowerCase()))
-				return true ;
+		
+		// blather monitor housekeeping
+		if (blatherer.equalsIgnoreCase(m.sender)) {
+			kipismCount += score(m.trailing, kipisms) ;
+			blatherCount++;
+		} else {
+			blatherer = m.sender ;
+			blatherCount = 1;
+			kipismCount = 0;
+		}		
+		// blatherer shutting-up
+		if (blatherCount >= blatherThreshold) {
+			// feel free to tweak the formula below
+			if (random.nextInt(100) < 20 + (blatherCount - blatherThreshold)*15 + kipismCount*30)
+				m.createReply(m.sender + ", " + pickRandom(responses)).send();
 		}
-		return false ;
 	}
 
-	private String pickRandom(ArrayList<String> strings) {
-		return strings.get(random.nextInt(strings.size())) ;
+	private int score(String target, String[] triggers) {
+		int ret = 0;
+		for(int i=0;i<triggers.length;i++) {
+			if (target.toLowerCase().contains(triggers[i].toLowerCase()))
+				ret++ ;
+		}
+		return ret;
+	}
+
+	private String pickRandom(String[] strings) {
+		return strings[random.nextInt(strings.length)] ;
 	}
 }
