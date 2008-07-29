d4e24e2ef1a1f9482de4c8105452b8aa3a96b9d1
robotslave <robotslave@gmail.com>
2008-07-29 01:26:47 +0000

    minor fixes, tweaks, aliases

--- src/goat/module/CurrencyConverter.java
+++ src/goat/module/CurrencyConverter.java
@@ -84,79 +84,65 @@ public class CurrencyConverter extends Module {
      * The last exchange rates table publication date.
      */
     private static String s_date = "";
+    
+    private static boolean DEBUG = true;
 
     public void processPrivateMessage(Message m) {
         processChannelMessage(m);
     }
 
     public void processChannelMessage(Message m) {
-        if (!s_date.equals(new Date())) {
+    	if (DEBUG) {
+    		System.out.println("convert: today's date is \"" + todaysDate() + "\"");
+    		System.out.println("convert: rate-table's date is \"" + s_date + "\"");
+    	}
+        if (!s_date.equals(todaysDate())) {
             EXCHANGE_RATES.clear();
         }
         if (EXCHANGE_RATES.isEmpty()) {
-            try {
-                final SAXBuilder builder = new SAXBuilder();
-                builder.setIgnoringElementContentWhitespace(true);
-
-                final Document doc = builder.build(new URL(EXCHANGE_TABLE_URL));
-                final Element root = doc.getRootElement();
-                final Namespace ns = root.getNamespace("");
-                final Element cubeRoot = root.getChild("Cube", ns);
-                final Element cubeTime = cubeRoot.getChild("Cube", ns);
-
-                s_date = cubeTime.getAttribute("time").getValue();
-
-                final List cubes = cubeTime.getChildren();
-                Element cube;
-
-                for (Object cube1 : cubes) {
-                    cube = (Element) cube1;
-                    EXCHANGE_RATES.put(cube.getAttribute("currency").getValue(), cube.getAttribute("rate").getValue());
-                }
-
-                EXCHANGE_RATES.put("EUR", "1");
-            } catch (JDOMException e) {
-                m.createReply("An error has occurred while parsing the exchange rates table.").send();
-                e.printStackTrace();
-            } catch (IOException e) {
-                m.createReply("An error has occurred while fetching the exchange rates table:  " + e.getMessage()).send();
-                e.printStackTrace();
-            }
+        	updateRates(m);
+        	if (DEBUG)
+        		System.out.println("convert: new rate-table's date is \"" + s_date + "\"");
         }
-        System.out.println("1");
+        // System.out.println("1");
         m.modTrailing = m.modTrailing.toLowerCase();
+        
+        // a few aliases... be sure to keep things lower case in this section
+        m.modTrailing = m.modTrailing.replaceAll("gay money", "eur");
+        m.modTrailing = m.modTrailing.replaceAll("real money", "gbp");
+        m.modTrailing = m.modTrailing.replaceAll("proper money", "gbp");
+        m.modTrailing = m.modTrailing.replaceAll("blood money", "usd");
+        m.modTrailing = m.modTrailing.replaceAll("oil money", "usd");
+        m.modTrailing = m.modTrailing.replaceAll("dirty money", "usd");
+        m.modTrailing = m.modTrailing.replaceAll("eddie money", "usd");
+        m.modTrailing = m.modTrailing.replaceAll("monopoly money", "cad");
+        m.modTrailing = m.modTrailing.replaceAll("tubgirl money", "jpy");
+        
         if (!EXCHANGE_RATES.isEmpty()) {
-            if (m.modTrailing.matches(".*\\d+([,\\d]+)?(\\.\\d+)? [a-z]{3} to [a-z]{3}.*")) {
-                final String[] cmds = m.modTrailing.split(" ");
-
-                if (cmds.length == 4) {
-                    if (cmds[3].equals(cmds[1])) {
-                        m.createReply("You're kidding, right?").send();
-                    } else {
-                        try {
-                            final double amt = Double.parseDouble(cmds[0].replaceAll(",", ""));
-                            final double from = Double.parseDouble((String) EXCHANGE_RATES.get(cmds[1].toUpperCase()));
-                            final double to = Double.parseDouble((String) EXCHANGE_RATES.get(cmds[3].toUpperCase()));
-
-                            m.createPagedReply(NumberFormat.getCurrencyInstance(Locale.getDefault()).format(amt).substring(1) + ' ' +
-                                    cmds[1].toUpperCase() + " = " +
-                                    NumberFormat.getCurrencyInstance(Locale.getDefault()).format((amt * to) / from)
-                                    .substring(1) + ' ' + cmds[3].toUpperCase()).send();
-                        } catch (NullPointerException e) {
-                            m.createReply("The supported currencies are: " + EXCHANGE_RATES.keySet().toString()).send();
-                        } catch (NumberFormatException nfe) {
-                            m.createReply("There's something funky about that number that confuses me.").send();
-                        }
-                    }
+        	String fromCurrency="";
+        	double fromAmount = 1.0;
+        	String toCurrency="";
+        	if(DEBUG)
+        		System.out.println("convert: modTrailing is \"" + m.modTrailing + "\"");
+            if (m.modTrailing.matches("\\s*\\d+([,\\d]+)?(\\.\\d+)?\\s+[a-z]{3}\\s+to\\s+[a-z]{3}.*")) {
+                final String[] cmds = m.modTrailing.split("\\s+");
+                fromCurrency = cmds[1];
+                toCurrency = cmds[3];
+                try {
+                	fromAmount = Double.parseDouble(cmds[0].replaceAll(",", ""));
+                } catch (NumberFormatException nfe) {
+                    m.createReply("There's something funky about that number that confuses me.").send();
+                    return;
                 }
+            } else if(m.modTrailing.matches("\\s*[a-z]{3}\\s+to\\s+[a-z]{3}.*")) {
+            	final String[] cmds = m.modTrailing.split("\\s+");
+            	fromCurrency = cmds[0];
+            	toCurrency = cmds[2];
             } else if (m.modTrailing.trim().toLowerCase().equals(RATES_KEYWORD)) {
                 m.createReply("Last Update: " + s_date).send();
-
                 final Iterator it = EXCHANGE_RATES.keySet().iterator();
                 String rate;
-
                 final StringBuffer buff = new StringBuffer(0);
-
                 while (it.hasNext()) {
                     rate = (String) it.next();
                     if (buff.length() > 0) {
@@ -164,12 +150,25 @@ public class CurrencyConverter extends Module {
                     }
                     buff.append(rate).append(": ").append(EXCHANGE_RATES.get(rate));
                 }
-
-                m.createReply(buff.toString()).send();
-
+                m.createPagedReply(buff.toString()).send();
+                return;
             } else {
-                m.createReply("The supported currencies are: " + EXCHANGE_RATES.keySet().toString());
+                m.createPagedReply("The supported currencies are: " + EXCHANGE_RATES.keySet().toString()).send();
+                return;
             }
+            
+            if (fromCurrency.equalsIgnoreCase(toCurrency))
+            	m.createReply("I'm not converting from " + fromCurrency.toUpperCase() + " to " + toCurrency.toUpperCase() + ", duh.").send();
+            else
+            	try {
+            		NumberFormat nf = NumberFormat.getInstance();
+            		nf.setMaximumFractionDigits(3);
+            		m.createReply(fromAmount + " " + fromCurrency.toUpperCase() + " = " +  nf.format(convert(fromAmount, fromCurrency, toCurrency)) + " " + toCurrency.toUpperCase()).send();
+            	} catch (NullPointerException e) {
+                    m.createPagedReply("The supported currencies are: " + EXCHANGE_RATES.keySet().toString()).send();
+                } catch (NumberFormatException nfe) {
+                    m.createReply("I got confused by a number in my exchange rate table.").send();
+                }
         } else {
             m.createReply("Sorry, but the exchange rate table is empty.").send();
         }
@@ -177,5 +176,52 @@ public class CurrencyConverter extends Module {
 
     public static String[] getCommands() {
         return new String[]{"convert"};
+    }
+    
+    private static Double convert(Double amount, String fromCurrency, String toCurrency) throws NullPointerException, NumberFormatException {
+    	
+    	if(DEBUG)
+    		System.out.println("convert:  converting " + amount + " " + fromCurrency + " to " + toCurrency);
+    	final double from = Double.parseDouble((String) EXCHANGE_RATES.get(fromCurrency.toUpperCase()));
+    	final double to = Double.parseDouble((String) EXCHANGE_RATES.get(toCurrency.toUpperCase()));
+
+    	return amount * to / from ;
+    }
+    
+    private static void updateRates(Message m) {
+        try {
+            final SAXBuilder builder = new SAXBuilder();
+            builder.setIgnoringElementContentWhitespace(true);
+
+            final Document doc = builder.build(new URL(EXCHANGE_TABLE_URL));
+            final Element root = doc.getRootElement();
+            final Namespace ns = root.getNamespace("");
+            final Element cubeRoot = root.getChild("Cube", ns);
+            final Element cubeTime = cubeRoot.getChild("Cube", ns);
+
+            s_date = cubeTime.getAttribute("time").getValue();
+
+            final List cubes = cubeTime.getChildren();
+            Element cube;
+
+            for (Object cube1 : cubes) {
+                cube = (Element) cube1;
+                EXCHANGE_RATES.put(cube.getAttribute("currency").getValue(), cube.getAttribute("rate").getValue());
+            }
+
+            EXCHANGE_RATES.put("EUR", "1");
+        } catch (JDOMException e) {
+            m.createReply("An error has occurred while parsing the exchange rates table.").send();
+            e.printStackTrace();
+        } catch (IOException e) {
+            m.createReply("An error has occurred while fetching the exchange rates table:  " + e.getMessage()).send();
+            e.printStackTrace();
+        }
+    }
+    
+    private String todaysDate() {
+    	Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+    	cal.setTime(new Date());
+    	return cal.get(Calendar.YEAR) + "-" + (cal.get(Calendar.MONTH) + 1) + "-" + cal.get(Calendar.DAY_OF_MONTH);
     }
 }
