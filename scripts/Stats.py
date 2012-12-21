24cb2a00a037d21d022520b8586b560c03190608
bcorrigan <b.j.corrigan@gmail.com>
2012-12-21 09:36:39 +0000

    bugfixes, tidy ups. lb is the new qpt. Also gives you a clean bill of health if you are good.

--- scripts/Stats.py
+++ scripts/Stats.py
@@ -51,26 +51,56 @@ class Stats(Module):
 	user=parser.get("user")
         if(self.hasUserStore(user)):
           userStore=self.getUserStore(user)
-          m.reply(m.sender+": "+user+" has written " + str(userStore.get("linesSeen")) + \
-                  " lines with an average of " + self.getAvg(userStore) + " words per line. "+ \
-                    user+" has cursed "+str(userStore.get("curseCount"))+" times, a ratio of " + \
-                    self.getCurseAvg(userStore) + "." +" "+ user + " has indulged in race hatred " + \
-                    str(userStore.get("racistCount")) + " times, a ratio of " +\
-                    self.getRacistAvg(userStore) + ". " + \
-                    user + " has been in ugly homophobic incidents " + str(userStore.get("homoPhobiaCount"))  +\
-                    " times, a ratio of " + self.getHomoAvg(userStore) + ".")
+
+          reply = m.sender+": "+user+" has written " + str(userStore.get("linesSeen")) + " lines"
+          if(userStore.has("wordCount")):
+            reply += " with an average of " + self.getAvg(userStore) + " words per line. "
+          if(userStore.has("curseCount")):
+            if(userStore.get("curseCount")>0):
+              reply+=user+" has cursed "+str(userStore.get("curseCount"))+" times"
+              reply+=", a ratio of " + self.getCurseAvg(userStore) + ". " 
+            else:
+              reply+=user+" is clean of mouth. "
+          if(userStore.has("racistCount")):
+            if(userStore.get("racistCount")>0):
+              reply+=user + " has indulged in race hatred " + str(userStore.get("racistCount"))+" times"
+              reply+=", a ratio of "+self.getRacistAvg(userStore) + ". "
+            else:
+              reply+=user + " is colour blind."
+          if(userStore.has("homoPhobiaCount")):
+            if(userStore.get("homoPhobiaCount")>0):
+              reply+=user+" has been in ugly homophobic incidents "+str(userStore.get("homoPhobiaCount"))+" times"
+              reply+=", a ratio of " + self.getHomoAvg(userStore) + "."
+            else:
+              reply+=user+" is as cisfriendly as Dan Savage. "
+          m.reply(reply)
 	else:
 	  m.reply(m.sender+": I have never seen that person.")
       else:
         chanStore=self.getChanStore(m)
-	m.reply("I have seen:" + str(chanStore.get("linesSeen")) + " messages in " + m.getChanname() + \
-                " and " + str(chanStore.get("wordCount")) + " words." + \
-                " The channel has cursed "+str(chanStore.get("curseCount"))+" times, a ratio of " + \
-                    self.getCurseAvg(chanStore)+"." +" "+ m.getChanname() + " has indulged in race hatred " + \
-                    str(chanStore.get("racistCount")) + " times, a ratio of " +\
-                    self.getRacistAvg(chanStore) + ". " + \
-                    m.getChanname() + " has seen homophobic incidents " + str(chanStore.get("homoPhobiaCount"))  +\
-                    " times, a ratio of " + self.getHomoAvg(chanStore) + ".")
+        reply = m.sender+": "+m.getChanname()+" has seen " + str(chanStore.get("linesSeen")) + " lines"
+        if(chanStore.has("wordCount")):
+          reply += " with an average of " + self.getAvg(chanStore) + " words per line. "
+        if(chanStore.has("curseCount")):
+          if(chanStore.get("curseCount")>0):
+            reply+=m.getChanname()+" has seen curses "+str(chanStore.get("curseCount"))+" times"
+            reply+=", a ratio of " + self.getCurseAvg(chanStore) + ". " 
+          else:
+            reply+=m.getChanname()+" needs to get out more. "
+        if(chanStore.has("racistCount")):
+          if(chanStore.get("racistCount")>0):
+            reply+=m.getChanname() + " has seen race hatred " + str(chanStore.get("racistCount"))+" times"
+            reply+=", a ratio of "+self.getRacistAvg(chanStore) + ". "
+          else:
+            reply+=m.getChanname() + " is colour blind."
+        if(chanStore.has("homoPhobiaCount")):
+          if(chanStore.get("homoPhobiaCount")>0):
+            reply+=m.getChanname()+" has seen ugly homophobic incidents "+str(chanStore.get("homoPhobiaCount"))+" times"
+            reply+=", a ratio of " + self.getHomoAvg(chanStore) + "."
+          else:
+            reply+=m.getChanname()+" is as cisfriendly as Dan Savage. "
+        m.reply(reply)
+
     else:
       self.updateStats(m)
     
@@ -79,9 +109,10 @@ class Stats(Module):
 
   def incSave(self, store, propName, inc=1):
     if(store.has(propName)):
-      prop=store.get(propName)
-      prop+=inc
-      store.save(propName,prop)
+      if(inc>0):
+        prop=store.get(propName)
+        prop+=inc
+        store.save(propName,prop)
     else:
       store.save(propName,inc)
     
@@ -107,13 +138,13 @@ class Stats(Module):
     return '{0:.3%}'.format(userStore.get("homoPhobiaCount")/float(userStore.get("wordCount")))
 
   def countCurses(self,m):
-    return len(re.findall(r'fuck|cunt|shit|piss|fag|jizz|cock|tits|pussy', m.getTrailing().lower()))
+    return len(re.findall(r'fuck[a-z]*|cunt[a-z]*|shit[a-z]*|piss[a-z]*|fag[a-z]*|jizz[a-z]*|cock[a-z]*|tits[a-z]*|pussy[a-z]*', m.getTrailing().lower()))
 
   def countRacisms(self,m):
-    return len(re.findall(r'kike|chink|paki|nigger|spic|gook|boche|wetback|kraut|honkey|porch monkey|raghead|anglo|camel jockey|darkie|greaser|jew|paddy|mick|pikey|fenian|gypsy|eskimo|shylock|musselman|moslem|mosselman', m.getTrailing().lower()))
+    return len(re.findall(r'kike[a-z]*|chink[a-z]*|paki[a-z]*|nigger[a-z]*|spic[a-z]*|gook[a-z]*|boche[a-z]*|wetback[a-z]*|kraut[a-z]*|honkey[a-z]*|porch monkey|raghead[a-z]*|anglo[a-z]*|camel jockey|darkie[a-z]*|greaser[a-z]*|jew[a-z]*|paddy[a-z]*|paddie[a-z]*|mick[a-z]*|pikey[a-z]*|fenian[a-z]*|gypsy[a-z]*|eskimo[a-z]*|shylock[a-z]*|musselman[a-z]*|moslem[a-z]*|mosselman[a-z]*', m.getTrailing().lower()))
 
   def countHomophobia(self,m):
-    return len(re.findall(r'fag|gaylord|fudgepack|fudge pack|tranny|queer|homo|cocksucker|buttrammer|poof|womyn|sodomite|dyke|carpetmunch|carpet munch|butch lesb|frigid|muff diver', m.getTrailing().lower()))
+    return len(re.findall(r'fag[a-z]*|gaylord[a-z]*|fudgepack[a-z]*|fudge pack[a-z]*|tranny[a-z]*|queer[a-z]*|homo[a-z]*|cocksucker[a-z]*|buttrammer[a-z]*|poof[a-z]*|womyn[a-z]*|sodomite[a-z]*|dyke[a-z]*|carpetmunch[a-z]*|carpet munch[a-z]*|butch lesb[a-z]*|frigid[a-z]*|muff diver[a-z]*', m.getTrailing().lower()))
 
 #This should always return a new instance
 def getInstance():
