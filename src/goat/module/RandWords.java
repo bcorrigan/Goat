8b847368b0e3b77fe7c55e5844dfe9ec56179e8a
edslocomb <edslocomb@gmail.com>
2012-12-22 08:11:28 -0800

    emoji ^_^

--- src/goat/module/RandWords.java
+++ src/goat/module/RandWords.java
@@ -2,15 +2,16 @@ package goat.module;
 
 import goat.core.Module;
 import goat.core.Message;
-import goat.util.Dict ;
-import goat.util.CommandParser ;
+import goat.util.Dict;
+import goat.util.CommandParser;
+import static goat.core.Constants.*;
 
-import java.util.ArrayList ;
-import java.util.Iterator ;
-import java.util.Random ;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 /**
- * Title:    
+ * Title:
  * Copyright (c) 2004 Robot Slave Enterprise Solutions
  * <p/>
  * @author encontrado
@@ -22,14 +23,23 @@ public class RandWords extends Module {
 
 	private Random random = new Random() ;
 	private Dict dict = new Dict() ;
-	
+
 	public int messageType() {
 		return WANT_COMMAND_MESSAGES;
 	}
    public String[] getCommands() {
-		return new String[]{"randword", "randwords", "bandname", "headline"};
+       return new String[]{
+           "randword",
+           "randwords",
+           "bandname",
+           "headline",
+           "emoji",
+           "goatji",
+           "brofist",
+           "slowclap",
+       };
    }
-	
+
 	public RandWords() {
 	}
 
@@ -39,13 +49,13 @@ public class RandWords extends Module {
 
 	public void processChannelMessage(Message m) {
 		int num = 1 ;
-		if (m.getModCommand().equalsIgnoreCase("randword") 
+		if (m.getModCommand().equalsIgnoreCase("randword")
 				|| m.getModCommand().equals("randwords")) {
 			String numString = "";
 			CommandParser parser = new CommandParser(m) ;
-			if (parser.hasVar("num")) 
+			if (parser.hasVar("num"))
 				numString = parser.get("num") ;
-			else if (parser.remaining().matches("^\\d+$")) 
+			else if (parser.remaining().matches("^\\d+$"))
 				numString = parser.remaining() ;
 			if (! numString.equals(""))
 				try {
@@ -65,7 +75,7 @@ public class RandWords extends Module {
 				return ;
 			} else if (num > 30) {
 				num = 30 ;
-			}		
+			}
 			m.reply(randWordString(num)) ;
 		} else if (m.getModCommand().equalsIgnoreCase("bandname")) {
 			String arg = m.getModTrailing().trim() ;
@@ -98,7 +108,23 @@ public class RandWords extends Module {
 				reply += seeds.remove(0) ;
 			}
 			m.reply(reply) ;
-		}
+		} else if (m.getModCommand().equalsIgnoreCase("emoji")) {
+                    m.reply(randEmojiWithName());
+                } else if (m.getModCommand().equalsIgnoreCase("goatji")) {
+                    m.reply(new String(Character.toChars(128016)));
+                } else if (m.getModCommand().equalsIgnoreCase("brofist")) {
+                    m.reply(new String(Character.toChars(128074)));
+                } else if (m.getModCommand().equalsIgnoreCase("slowclap")) {
+                    try {
+                        Thread.sleep(1500);
+                        m.reply(new String(Character.toChars(128079)));
+                        Thread.sleep(2000);
+                        m.reply("  " + new String(Character.toChars(128079)));
+                        Thread.sleep(2500);
+                        m.reply("  " + NORMAL + "  " + new String(Character.toChars(128079)));
+                    } catch(InterruptedException tie) {
+                    }
+                }
 	}
 
 	public String randWordString(int num) {
@@ -120,7 +146,7 @@ public class RandWords extends Module {
 			return returnpoop.trim() ;
 		}
 	}
-	
+
 	public String getWord() {
 		// return one random word as a string
 		return dict.getWord(random.nextInt(dict.numWords)) ;
@@ -134,4 +160,21 @@ public class RandWords extends Module {
 		}
 		return wordList ;
 	}
+
+    static ArrayList<Integer> emoji = new ArrayList<Integer>();
+
+    private void initEmoji() {
+        int ranges[][] = {{127744, 128511}, {128512, 128591}, {128640, 128767}, {9728, 9983}};
+        for (int j = 0; j < ranges.length; j++)
+            for (int i = ranges[j][0]; i <= ranges[j][1]; i++)
+                if (Character.isDefined(i))
+                    emoji.add(i);
+    }
+
+    private String randEmojiWithName() {
+        if (emoji.isEmpty())
+            initEmoji();
+        int ch = emoji.get(random.nextInt(emoji.size()));
+        return new String(Character.toChars(ch)) + "  " + Character.getName(ch);
+    }
 }
