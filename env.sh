530dd14e56f5ef8972e11ce28c114d74d32be25f
lb <jon@burdge.org>
2012-12-28 14:10:23 -0800

    New space age init script.  env.sh should get updated for goathost as the standard.

---
+++ env.sh
@@ -0,0 +1,8 @@
+# this is your git root.
+GOAT_HOME=/home/jon/git/goat
+
+# the java binary
+JAVA=/usr/lib/jvm/java-7-openjdk-amd64/bin/java
+
+# this is the path to scala-library.jar
+SCALA_LIB=/usr/share/java/scala-library.jar
--- goat.sh
+++ goat.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+# goat's space age initialization script
 
-# stinky one-liner to run goat.
+# load environment config
+source env.sh
 
-# runs goat via jar file
-#java -server -Xbootclasspath/a:`echo lib/* | tr \  :` -jar goat.jar
+# environment variables for goat
+export JYTHONPATH=$GOAT_HOME/libpy
 
 # runs goat by specifying Goat class (still uses jar file, but only as lib)
-java -server -XX:+UseParallelGC -XX:+UseCompressedOops -Xbootclasspath/a:`echo lib/*.jar | tr \  :`:$SCALA_HOME/lib/scala-library.jar:goat.jar goat.Goat
+$JAVA \
+    -server \
+    -XX:+UseParallelGC \
+    -XX:+UseCompressedOops \
+    -Xbootclasspath/a:`echo lib/*.jar | tr " " :`:$SCALA_HOME/scala-library.jar:goat.jar \
+    goat.Goat
