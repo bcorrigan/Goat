cab85b25dbc860221814d903c420ad6e32f8765e
edslocomb <edslocomb@gmail.com>
2013-01-26 14:39:34 -0800

    use CommandParser.getNumber in rottentomatoes

--- src/goat/module/RottenTomatoes.scala
+++ src/goat/module/RottenTomatoes.scala
@@ -14,16 +14,16 @@ import scala.collection.JavaConversions._
 class RottenTomatoes extends Module {
 
   val apiKey = "qdcearnjeudmt6kq5kmtsq5w"
-  
+
   val api = new RottenTomatoesApi(apiKey)
-  
+
   private val STAR="\u2605"    // "★"
   private val STAR_12="\u272B" // "✫"
   private val STAR_34="\u2730" // "✰"
   private val STAR_14="\u2606" // "☆"
-  
+
   var results:Seq[RTMovie] = Nil;
-  
+
   def getCommands(): Array[String] = { Array("films","film") }
 
   def processPrivateMessage(m: Message): Unit = {
@@ -32,15 +32,14 @@ class RottenTomatoes extends Module {
 
   def processChannelMessage(m: Message): Unit = {
     val parser = new CommandParser(m);
-    
+
     if(parser.hasWord("boxoffice") || parser.hasWord("top")) {
       results = api.getInTheaters()
       showResults(m, "Top box office:")
-    } else if(parser.hasVar("num") && !parser.hasRemaining()) {
-        var num=sanitiseAndScoldForNum(m,parser)
-        if(num!=0) {
-        	showFilm(m,num-1)
-        }
+    } else if(parser.hasOnlyNumber) {
+        val num = sanitiseAndScoldForNum(m,parser)
+        if(num != 0)
+          showFilm(m, num - 1)
     } else if(parser.hasWord("dvd") || parser.hasWord("pirate")) {
       var lead=""
       if(parser.hasWord("new")) {
@@ -58,30 +57,37 @@ class RottenTomatoes extends Module {
       results=api.getMoviesSearch(parser.remainingAfterWord("search"))
       showResults(m,"Results");
     } else if(parser.hasWord("similar")) {
-      if(parser.hasVar("num")) {
-        var num=sanitiseAndScoldForNum(m,parser)
-        if(num!=0) {
-        	results = api.getMoviesSimilar(results.get(num-1).getId())
+      if(parser.hasNumber) {
+        val num = sanitiseAndScoldForNum(m,parser)
+        if(num != 0) {
+        	results = api.getMoviesSimilar(results.get(num - 1).getId())
         	showResults(m, "Similar")
         }
       } else {
         m.reply(m.getSender() + ": Similar to what?")
-      } 
+      }
     } else if(parser.hasWord("help")) {
       m.reply("Usage: films [search] query | [to pirate] | [new to pirate] | [boxoffice] | upcoming.  film num=N to view a film in result set.");
     }
   }
-  
-  private def sanitiseAndScoldForNum(m:Message, parser:CommandParser):Int = {
-    var num=parser.getInt("num")
-    
-    if(results.length<num || num<0) {
-      m.reply("You're not being funny, you know.") 
-      return 0;
+
+  private def sanitiseAndScoldForNum(m:Message, parser:CommandParser):Int =
+    try {
+      val doubleNum = parser.findNumber
+      val num = if (doubleNum > Int.MaxValue || doubleNum < Int.MinValue)
+        { m.reply("You sir are a swinish lout."); 0 }
+      else
+        doubleNum.toInt
+
+      if(results.length < num || num <= 0)
+        { m.reply("You're not being funny, nerd."); 0 }
+      else
+        num
+    } catch {
+      case nfe: NumberFormatException =>
+        m.reply("I don't believe that's a number"); 0
     }
-    return num;
-  }
-  
+
   private def showResults(m:Message, lead:String):Unit = {
       if(results.length==1)
         showFilm(m,0)
@@ -100,11 +106,11 @@ class RottenTomatoes extends Module {
         m.reply(lead+", " + reply);
     }
   }
-  
+
   private def showFilm(m:Message, num:Int):Unit = {
     if(results.length>0) {
       var movie = results.get(num);
-      var reply = BOLD + movie.getTitle() + BOLD + "(" + movie.getYear() + ")" 
+      var reply = BOLD + movie.getTitle() + BOLD + "(" + movie.getYear() + ")"
       if(movie.getCertification!=null && !movie.getCertification.equals(""))
         reply+=", certified " + movie.getCertification()
       if(movie.getDirectors().size()>0)
@@ -114,7 +120,7 @@ class RottenTomatoes extends Module {
       if(movie.getStudio()!=null)
     	  reply+= " Studio:" + movie.getStudio()
       if(movie.getRatings().size()>0)
-        reply+=getRating(movie) 
+        reply+=getRating(movie)
       if(movie.getGenres().size()>0)
     	  reply += movie.getGenres().mkString(",")
       if(movie.getCriticsConsensus()!=null && !movie.getCriticsConsensus().equals(""))
@@ -126,27 +132,25 @@ class RottenTomatoes extends Module {
       m.reply("You have to search for a film, before I can show you a film, foolish child.")
     }
   }
-  
+
   //critics_rating:Certified Fresh, critics_score:73, audience_rating:Upright, audience_score:84
   private def getRating(film:RTMovie):String = {
-    var rating=""
-    var score = getRatings(film)
+    val score = getRatings(film)
 
-    if(score.ponceFactor.isDefined)
-    	rating+=" ponceFactor:" + score.ponceFactorString.get
-    
-    
-    if(score.cS>0)
-    	rating+=" critics:" + score.cS 
-    if(score.aS>0)	
-    	rating+=" plebs:" + score.aS
-    
-    rating
+    { if(score.ponceFactor.isDefined)
+      " ponceFactor:" + score.ponceFactorString.get
+    else "" } +
+    { if(score.cS>0)
+      " critics:" + score.cS
+    else "" } +
+    { if(score.aS>0)
+      " plebs:" + score.aS
+    else "" }
   }
-  
+
   private def getRatings(film:RTMovie):Ratings = {
     val ratings=film.getRatings().toMap
-    
+
     var cS,aS = 0
     var aR,cR = "";
     for(r<-ratings) {
@@ -155,12 +159,12 @@ class RottenTomatoes extends Module {
       	case "critics_score" => cS=r._2.toInt
       	case "audience_score" => aS=r._2.toInt
       	case "audience_rating" => aR=r._2
-      	case _ => 
+      	case _ =>
       }
     }
     new Ratings(cS,aS,aR,cR);
   }
-  
+
   private def getCondensedRating(film:RTMovie):String = {
     var score = getRatings(film);
     var condensedRating=""
@@ -170,24 +174,24 @@ class RottenTomatoes extends Module {
       condensedRating+=RED+"C"+NORMAL
     else if(score.cS<=75&&score.cS>=50)
       condensedRating+="C"
-    
+
     if(score.aS>75)
       condensedRating+=GREEN+"P"+NORMAL
     else if(score.aS<50&&score.aS>0)
       condensedRating+=RED+"P"+NORMAL
     else if(score.aS<=75&&score.aS>=50)
       condensedRating+="P"
-    
+
     condensedRating+=score.ponceFactorString.getOrElse("")
-        
+
     condensedRating
   }
-  
+
   private case class Ratings(cS:Int,aS:Int,aR:String,cR:String) {
     val ponceFactor:Option[Double] = if(cS>0&&aS>0) Some(cS/aS.toDouble) else None
     val ponceFactorString:Option[String] = if(ponceFactor.isDefined) Some("%1.2f" format ponceFactor.get) else None
   };
-  
+
   private def getStarRating(film:RTMovie):String = {
     var score = getRatings(film)
     var starRating=""
@@ -198,7 +202,7 @@ class RottenTomatoes extends Module {
     }
     return starRating
   }
-  
+
   private def stepToStar(step:Int):String = {
     step match {
       case 20 => return STAR
@@ -208,7 +212,7 @@ class RottenTomatoes extends Module {
       case _ => return ""
     }
   }
-  
+
   private def extractStars(rating:Int,step:Int):Tuple2[Int,String] = {
     var r=rating;
     var stars:String="";
@@ -216,20 +220,20 @@ class RottenTomatoes extends Module {
       r-=step
       stars+=stepToStar(step);
     }
-    
+
     return new Tuple2(r,stars)
   }
-  
+
   private def ratingToStars(rating:Int):String = {
     var r=rating
     var stars=""
-    
+
     List(20,15,10,5) foreach { step=>
       var t=extractStars(r,step)
       stars+=t._2
       r=t._1
     }
-      
+
     return stars
   }
 
