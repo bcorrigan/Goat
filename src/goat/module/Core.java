5c0d925f7839d17ea4c95df70cf082b3f80f3f6e
edslocomb <edslocomb@gmail.com>
2013-01-26 13:19:42 -0800

    gratuitous reindent

--- src/goat/module/Core.java
+++ src/goat/module/Core.java
@@ -15,62 +15,62 @@ import goat.core.BotStats;
  */
 public class Core extends Module {
 
-	public void processPrivateMessage(Message m) {
-		if (m.isAuthorised()) {
-			if (m.getModCommand().toLowerCase().equals("part")) {
-				if (BotStats.getInstance().containsChannel(m.getModTrailing())) {
-					new Message("", "PART", m.getModTrailing(), "").send();
-					m.reply("Channel " + m.getModTrailing() + " parted!");
-					BotStats.getInstance().removeChannel(m.getModTrailing());
-					return;
-				}
-				m.reply("I'm not on any such channel " + m.getModTrailing() + " :(");
+    public void processPrivateMessage(Message m) {
+        if (m.isAuthorised()) {
+            if (m.getModCommand().toLowerCase().equals("part")) {
+                if (BotStats.getInstance().containsChannel(m.getModTrailing())) {
+                    new Message("", "PART", m.getModTrailing(), "").send();
+                    m.reply("Channel " + m.getModTrailing() + " parted!");
+                    BotStats.getInstance().removeChannel(m.getModTrailing());
+                    return;
+                }
+                m.reply("I'm not on any such channel " + m.getModTrailing() + " :(");
 
-			}
-			else if (m.getModCommand().toLowerCase().equals("join"))
-				if (BotStats.getInstance().isValidChannelName(m.getModTrailing())) {
-					new Message("", "JOIN", m.getModTrailing(), "").send();
-					String response = "Channel " + m.getModTrailing() + " joined!" ;
-					// we do this next dodge to avoid sending messages to nobody on startup
-					if (m.getReplyTo().equals(""))
-						System.out.println(response) ;
-					else
-						m.reply(response);
-					BotStats.getInstance().addChannel(m.getModTrailing());
-				} else
-					m.reply("Sorry, that's not a valid channel name!");
-			else if (m.getModCommand().toLowerCase().equals("nick"))
-				new Message("", "NICK", m.getModTrailing(), "").send();
-			else if (m.getModCommand().toLowerCase().equals("quit")) {
-				new Message("", "QUIT", m.getModTrailing(), "").send();     //@TODO not sending the quit message properly!
-				System.exit(0);
-				}
-			else if (m.getModCommand().toLowerCase().equals("charset")) {
-				Charset charset;
-				try { 
-					charset = Charset.forName( m.getModTrailing().trim() );
-				} catch(IllegalCharsetNameException icne) {
-					m.reply("That charset is illegally specified :(");
-					return;
-				} catch(UnsupportedCharsetException uce) {
-					m.reply("That charset is not supported in this JVM.");
-					return;
-				}
-				BotStats.getInstance().setCharset(charset);
-				m.reply("OK, changed to " + m.getModTrailing().trim() + " charset.");
-				}
-			}
-		if (m.getModCommand().toLowerCase().equals("showcharset")) {
-			m.reply( "Current charset is " + BotStats.getInstance().getCharset().toString());
-		}
-	}
+            }
+            else if (m.getModCommand().toLowerCase().equals("join"))
+                if (BotStats.getInstance().isValidChannelName(m.getModTrailing())) {
+                    new Message("", "JOIN", m.getModTrailing(), "").send();
+                    String response = "Channel " + m.getModTrailing() + " joined!" ;
+                    // we do this next dodge to avoid sending messages to nobody on startup
+                    if (m.getReplyTo().equals(""))
+                        System.out.println(response) ;
+                    else
+                        m.reply(response);
+                    BotStats.getInstance().addChannel(m.getModTrailing());
+                } else
+                    m.reply("Sorry, that's not a valid channel name!");
+            else if (m.getModCommand().toLowerCase().equals("nick"))
+                new Message("", "NICK", m.getModTrailing(), "").send();
+            else if (m.getModCommand().toLowerCase().equals("quit")) {
+                new Message("", "QUIT", m.getModTrailing(), "").send();     //@TODO not sending the quit message properly!
+                System.exit(0);
+            }
+            else if (m.getModCommand().toLowerCase().equals("charset")) {
+                Charset charset;
+                try {
+                    charset = Charset.forName( m.getModTrailing().trim() );
+                } catch(IllegalCharsetNameException icne) {
+                    m.reply("That charset is illegally specified :(");
+                    return;
+                } catch(UnsupportedCharsetException uce) {
+                    m.reply("That charset is not supported in this JVM.");
+                    return;
+                }
+                BotStats.getInstance().setCharset(charset);
+                m.reply("OK, changed to " + m.getModTrailing().trim() + " charset.");
+            }
+        }
+        if (m.getModCommand().toLowerCase().equals("showcharset")) {
+            m.reply( "Current charset is " + BotStats.getInstance().getCharset().toString());
+        }
+    }
 
 
-	public void processChannelMessage(Message m) {
-		processPrivateMessage(m);
-	}
+    public void processChannelMessage(Message m) {
+        processPrivateMessage(m);
+    }
 
-	public String[] getCommands() {
-		return new String[]{"part", "join", "nick", "quit", "charset", "showcharset"};
-	}
+    public String[] getCommands() {
+        return new String[]{"part", "join", "nick", "quit", "charset", "showcharset"};
+    }
 }
