56433efab7ba8659382678028329c3a2bc0d19ab
edslocomb <edslocomb@gmail.com>
2013-01-26 14:00:28 -0800

    fix reauth so 'This' is no longer regarded as a command

--- src/goat/module/NickServ.java
+++ src/goat/module/NickServ.java
@@ -13,47 +13,43 @@ import java.io.*;
  */
 public class NickServ extends Module {
 
-	private long lastAuth;
+    private long lastReAuth;
 
-	public NickServ() {
-	    inAllChannels = true;
-	    new Message("", "PRIVMSG", "NickServ", "identify " + getPassword("irc.pass")).send();
-	}
+    public NickServ() {
+        inAllChannels = true;
+        new Message("", "PRIVMSG", "NickServ", "identify " + getPassword("irc.pass")).send();
+    }
 
     public void processPrivateMessage(Message m) {
         processChannelMessage(m);
-	}
+    }
 
     public void processOtherMessage(Message m) {
         processChannelMessage(m);
-	}
+    }
 
-	public void processChannelMessage(Message m) {
+    public void processChannelMessage(Message m) {
 
-	    //TODO really need notice functionality in this thing, or some better way of
-	    //  doing reauth
+        //TODO really need notice functionality in this thing, or some better way of
+        //  doing reauth
 
-	    //if our password is wrong we don't want to spam NickServ
-	    long now = System.currentTimeMillis();
-	    if( (now - lastAuth) > 30000)
-		if(m.getSender().equals("NickServ"))
-		    if(m.getPrefix().equals("NickServ!services@services.slashnet.org")) {
-			lastAuth = System.currentTimeMillis();
-			new Message("", "PRIVMSG", "NickServ", "identify " + getPassword("irc.pass")).send();
-		    }
-	}
+        //if our password is wrong we don't want to spam NickServ
+        long now = System.currentTimeMillis();
+        if( (now - lastReAuth) > 30000)
+            if(m.getSender().equals("NickServ"))
+                if (m.getTrailing().startsWith("This nickname is registered and protected."))
+                    if(m.getPrefix().equals("NickServ!services@services.slashnet.org")) {
+                        lastReAuth = System.currentTimeMillis();
+                        new Message("", "PRIVMSG", "NickServ", "identify " + getPassword("irc.pass")).send();
+                    }
+    }
 
-	public int messageType() {
-		return WANT_COMMAND_MESSAGES;
-	}
+    public int messageType() {
+        return WANT_ALL_MESSAGES;
+    }
 
-	public String[] getCommands() {
-	    // a bit fragile; we're counting on NickServ to begin its password challenge with the word "This"
-	    return new String[]{"This"};
-	}
+    public String[] getCommands() {
+        return new String[0];
+    }
 
-	public static void main(String[] args) {
-		NickServ ns = new NickServ();
-		System.out.println(getPassword("irc.pass"));
-	}
 }
