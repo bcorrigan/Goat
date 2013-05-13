a078ac983f537d6ce24f6ee52bf164fbb209446d
robotslave <robotslave@gmail.com>
2013-05-13 15:27:29 -0700

    Switch Define to use urbandictionary api v0 instead of scraping

--- src/main/java/goat/module/Define.java
+++ src/main/java/goat/module/Define.java
@@ -6,6 +6,7 @@ import goat.core.Module;
 import goat.core.Message;
 import goat.util.DICTClient;
 import goat.util.Definition;
+import goat.util.UrbanDictionary;
 import goat.util.CommandParser;
 import goat.module.WordGame;
 
@@ -34,9 +35,7 @@ import org.json.JSONObject;
 public class Define extends Module {
 
 	private static String host = "dict.org" ;
-	private static String urbandictionaryDescription = "The somewhat spotty slang dictionary at urbandictionary.com" ;
 	public boolean debug = false ;
-
 	public int messageType() {
 		return WANT_COMMAND_MESSAGES;
 	}
@@ -112,23 +111,16 @@ public class Define extends Module {
       Vector<Definition> definitionList = null ;
       String[][] matchList = null ;
 
-      // This next block is thuggish.  If we keep tacking dictionaries onto the project,
-      // we might want to rearrange stuff here and elsewhere to do things consistently
       if (dictionary.equalsIgnoreCase("urban")) {
-         definitionList = getUrbanDefinitions(word, m) ;
-         // this next is ugly, but urbandictionary.com doesn't have a guess-mis-spelt-word feature
-         if ((null == definitionList) || definitionList.isEmpty())
-            matchList =  new String[0][0];
-         else {
-            matchList = new String[1][1] ;
-            matchList[0][0] = word ;
-         }
+          UrbanDictionary urban = new UrbanDictionary(word);
+          if (!urban.error.equals("")) {
+              m.reply(urban.error);
+              return;
+          }
+          definitionList = urban.definitions;
+          matchList = urban.matchList;
       }
       else if (dictionary.equalsIgnoreCase("oed")) {
-         // If we were clever monkeys, we could get goat to ask bugmenot for
-         //   a login/password for oed.com, and then use that to get the page,
-         //   and then parse the page for definitionList and matchList.
-         //   instead, we'll just burp up a URL, and let the user do the legwork.
 			m.reply(oedUrl(word)) ;
 			return ;  //naughty!
 		}
@@ -230,227 +222,6 @@ public class Define extends Module {
 		return "http://dictionary.oed.com/cgi/findword?query_type=word&queryword=" + word.replaceAll(" ", "%20") ;
 	}
 
-	private String urbanUrl(String word) {
-		return "http://www.urbandictionary.com/define.php?term=" + word.replaceAll(" ", "%20") ;
-	}
-
-	public Vector<Definition> getUrbanDefinitions(String word) throws SocketTimeoutException {
-      Vector<Definition> definitionList = null;
-      HttpURLConnection connection = null;
-      try {
-         URL urban = new URL(urbanUrl(word));
-         connection = (HttpURLConnection) urban.openConnection();
-         /* incompatible with 1.4
-         * It seems java.net.Socket supports socket timeouts, but URLConnection does not expose the
-         * underlying socket's timeout ability before j2se1.5. So can either rework this code here to use Socket,
-         * or leave this commented out till 1.5 is released and more prevalent. Think this latter option is the best
-         * one (ie the one that involves least work).
-         */
-         connection.setConnectTimeout(3000);  //just three seconds, we can't hang around
-         if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            System.out.println("Fuckup at urbandictionary, HTTP Response code: " + connection.getResponseCode());
-            return null ;
-         }
-         if(debug)
-        	 System.out.println("Page retrieved from urbandictionary for word \"" + word + "\"") ;
-         definitionList = parseUrbanPage(new BufferedReader(new InputStreamReader(connection.getInputStream())));
-		} catch (SocketTimeoutException e) {
-			System.err.println("Connection to urbandictionary timed out.") ;
-			throw new SocketTimeoutException(e.toString()) ;
-      } catch (IOException e) {
-    	  if(debug)
-    		  System.err.println("IOException caught.") ;
-         e.printStackTrace();
-          return null ;
-      } finally {
-          if(connection!=null) connection.disconnect();
-      }
-      return definitionList ;
-   }
-
-	public Vector<Definition> getUrbanDefinitions(String word, Message m) {
-		try {
-			return getUrbanDefinitions(word) ;
-		} catch (SocketTimeoutException e) {
-			m.reply("Connection to urbandictionary timed out.") ;
-		}
-		return null;
-	}
-
-   private Vector<Definition> parseUrbanPage(BufferedReader br) throws IOException {
-		// In which we use java regexps, the painful way
-		Vector<Definition> definitionList = new Vector<Definition>();
-		String inputLine;
-		String word;
-		String definition = "";
-		String example = "";
-		int defNumber = -1;
-		Matcher matcher;
-
-		Pattern numberStartPattern = Pattern.compile("^\\s*<td class='index'>\\s*$");
-		Pattern numberBodyPattern = Pattern.compile("\\s*<a href=\"http:\\/\\/[a-z0-9-]+\\.urbanup\\.com\\/\\d+\">(\\d+)\\.<\\/a>\\s*");
-
-//		Pattern numberEndPattern = Pattern.compile("^\\s*</td>\\s*$");
-		Pattern wordStartPattern = Pattern.compile("^\\s*<td class='word'>\\s*$");
-//		Pattern wordEndPattern = numberEndPattern;
-//		Pattern def_pStartPattern = Pattern.compile("^\\s*<div class=\"def_p\">(.*)\\s*$");
-//		Pattern def_pBodyStartPattern = Pattern.compile("^\\s*<p>(.+?)(</p>)*\\s*$");
-//		Pattern def_pBodyEndPattern = Pattern.compile("(.*)</p>\\s*$") ;
-
-		Pattern definitionStartPattern = Pattern.compile("^\\s*<div class=\"definition\">\\s*(.*)\\s*$");
-		Pattern definitionEndPattern = Pattern.compile("^\\s*(.*?)\\s*</div>.*");
-
-		Pattern exampleStartPattern = Pattern.compile("<div class=\"example\">\\s*(.*)\\s*$");
-		Pattern exampleEndPattern = Pattern.compile("^\\s*(.*?)\\s*</div>\\s*$") ;
-
-		Pattern startPattern = numberStartPattern;
-		//Pattern endPattern = Pattern.compile("^\\s*</div>\\s*$");
-
-		// this has grown and grown, and now it is completely ludicrous.
-		while ((inputLine = br.readLine()) != null) {
-			// Do stuff...
-			matcher = startPattern.matcher(inputLine);
-			if (!matcher.find()) {
-				continue ;
-			} else { // word found
-				if(debug)
-					System.out.println("Start of definition block located.") ;
-				// parse out word number ;
-				matcher = numberBodyPattern.matcher(br.readLine());
-				matcher.find();
-				defNumber = Integer.parseInt(matcher.group(1));
-				// skip detection of numberEndPattern, not needed
-				if(debug)
-					System.out.println("  definition #" + defNumber) ;
-				// parse out word
-				matcher = wordStartPattern.matcher(br.readLine());
-				while (!matcher.find())
-					matcher = wordStartPattern.matcher(br.readLine());
-				word = br.readLine().trim(); // word or phrase should be alone on a line, no parse necessary
-				if(debug)
-					System.out.println("  word found: " + word) ;
-				// Definition and example have moved, now can be on same line.
-				// or not.  Go fuck yourself, urbandictionary.
-				String dline = "";
-				// parse out definition
-				definition = "";
-				dline = br.readLine();
-            matcher = definitionStartPattern.matcher(dline);
-            while(!matcher.find()) {
-					dline = br.readLine();
-               matcher = definitionStartPattern.matcher(dline);
-				}
-            definition = matcher.group(1);
-            matcher = definitionEndPattern.matcher(definition);
-            while(!matcher.find()) {
-					dline = br.readLine();
-               definition += " " + dline;
-               matcher = definitionEndPattern.matcher(definition);
-            }
-            definition = matcher.group(1);
-            // parse out example
-            example = "";
-            matcher = exampleStartPattern.matcher(dline);
-            while(!matcher.find()) {
-					dline = br.readLine();
-               matcher = exampleStartPattern.matcher(dline);
-				}
-            example = matcher.group(1);
-            matcher = exampleEndPattern.matcher(example);
-            while(!matcher.find()) {
-					dline = br.readLine();
-               example += " " + dline;
-               matcher = exampleEndPattern.matcher(example);
-            }
-            example = matcher.group(1);
-
-/* old site design made this hideous, rewriting from scratch
-
-				// parse out definition
-				String tempLine = br.readLine() ;
-				matcher = def_pStartPattern.matcher(tempLine);
-				while (!matcher.find()) {
-					tempLine = br.readLine() ;
-					matcher = def_pStartPattern.matcher(tempLine);
-				}
-				matcher = def_pBodyStartPattern.matcher(tempLine);
-				while(!matcher.find()) {
-					tempLine = br.readLine() ;
-					matcher = def_pBodyStartPattern.matcher(tempLine);
-				}
-				definition = matcher.group(1) ;
-				matcher = def_pBodyEndPattern.matcher(tempLine) ;
-				boolean multiline = false ;
-				while(!matcher.find()) {
-					if(multiline)
-						definition += tempLine ;
-					tempLine = br.readLine() ;
-					matcher = def_pBodyEndPattern.matcher(tempLine) ;
-					multiline = true ;
-				}
-				if(multiline)
-					definition += matcher.group(1);
-				if(debug)
-					System.out.println("  raw definition: " + definition);
-
-				// and example.
-				// this is ugly, as we don't want to run through to the next
-				// definition's examples, or EOF, if there are no examples for this def.
-				//
-
-				matcher = exampleStartPattern.matcher(tempLine) ;
-				Matcher endMatcher = endPattern.matcher(tempLine) ;
-				boolean definitionDone = false ;
-				while (true) {
-					if (matcher.find()) {
-						break ;
-					}
-					if (endMatcher.find()) {
-						definitionDone = true ;
-						break ;
-					}
-					tempLine = br.readLine() ;
-					matcher = exampleStartPattern.matcher(tempLine) ;
-					endMatcher = endPattern.matcher(tempLine) ;
-				}
-
-				if (!definitionDone) {
-					example = matcher.group(1);
-					matcher = exampleEndPattern.matcher(tempLine) ;
-					multiline = false ;
-					while(!matcher.find()) {
-						if (multiline)
-							example += tempLine ;
-						tempLine = br.readLine() ;
-						matcher = exampleEndPattern.matcher(tempLine) ;
-						multiline = true ;
-					}
-					if(multiline)
-						example += matcher.group(1);
-					if(debug)
-						System.out.println("  raw example: " + example);
-				}
-*/
-
-				// massage our definition into one line of readable ascii
-				if (!example.equals(""))
-					definition += Constants.BOLD + " Ex:" + Constants.NORMAL
-							+ " \"" + example + "\"";
-				definition = definition.replaceAll("\\n", " ");
-				definition = definition.replaceAll("&quot;", "\"");
-				definition = definition.replaceAll("&amp;", "&");
-				definition = definition.replaceAll("<br\\s*/>", "  ");
-				definition = definition
-						.replaceAll("<a .*?>", Constants.UNDERLINE);
-				definition = definition.replaceAll("</a>", Constants.NORMAL);
-				// insert new Definition object into definitionList
-				definitionList.addElement(new Definition("urban",
-						urbandictionaryDescription, word, definition));
-			}
-		}
-		return definitionList;
-	}
-
     private String dictionaries() {
     	String ret = "" ;
     	try {
@@ -488,7 +259,7 @@ public class Define extends Module {
          found = true ;
       }
       else if (code.equalsIgnoreCase("urban") ) {
-         line = "urban:  " + urbandictionaryDescription ;
+         line = "urban:  " + UrbanDictionary.dictionaryDescription ;
          found = true ;
       }
       else {
---
+++ src/main/java/goat/util/UrbanDictionary.java
@@ -0,0 +1,125 @@
+package goat.util;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.Vector;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import goat.core.BotStats;
+import goat.core.Constants;
+import goat.util.Definition;
+
+public class UrbanDictionary {
+
+    public Vector<Definition> definitions = new Vector<Definition>();
+    public String[][] matchList = new String[0][0];
+    public String error = "";
+    public static final String dictionaryDescription = "The somewhat spotty slang dictionary at urbandictionary.com" ;
+
+
+    public UrbanDictionary(String word) {
+        try {
+            parse(apiDefine(word));
+        } catch (JSONException je) {
+            error = "Could not parse JSON from UrbanDictionary:  " + je.getMessage();
+            je.printStackTrace();
+        }
+    }
+
+    private void parse(JSONObject json) throws JSONException {
+        if(json.has("error"))
+            error = json.getString("error");
+        else if(!json.has("result_type"))
+            error = "Response has no result type; I'm too confused to continue.";
+        else if(json.getString("result_type").equals("no_results"))
+            error = "No results found.";
+        else if(json.getString("result_type").equals("exact"))
+            definitions = buildDefinitions(json);
+        else if(json.getString("result_type").equals("fulltext"))
+            matchList = buildMatchList(json);
+        else
+            error = "Unknown result type:  \"" + json.get("result_type") + "\".";
+    }
+
+    public Vector<Definition> buildDefinitions(JSONObject json) throws JSONException {
+        Vector<Definition> ret = new Vector<Definition>();
+        if((json.has("result_type") && json.get("result_type").equals("exact"))) {
+            JSONArray jlist = json.getJSONArray("list");
+            for (int i = 0; i < jlist.length(); i++)
+                ret.add(new Definition("urban", dictionaryDescription,
+                                       jlist.getJSONObject(i).getString("word"),
+                                       jlist.getJSONObject(i).getString("definition") +
+                                       "  " + Constants.BOLD + "Example: " + Constants.NORMAL +
+                                       jlist.getJSONObject(i).getString("example")));
+
+        }
+        return ret;
+    }
+
+    public String[][] buildMatchList(JSONObject json) throws JSONException {
+        String[][] ret = new String[0][0];
+        if((json.has("result_type") && json.get("result_type").equals("fulltext"))) {
+            JSONArray jlist = json.getJSONArray("list");
+            ArrayList<String> matches = new ArrayList<String>();
+            for(int i = 0; i < jlist.length(); i++) {
+                String thisWord = jlist.getJSONObject(i).getString("word");
+                if(!matches.contains(thisWord))
+                    matches.add(thisWord);
+            }
+            ret = new String[matches.size()][2];
+            for(int j = 0; j < matches.size(); j++) {
+                ret[j][0] = "urban";
+                ret[j][1] = matches.get(j);
+            }
+        }
+        return ret;
+    }
+
+    private JSONObject apiDefine(String word) throws JSONException {
+        HttpURLConnection connection = null;
+        BufferedReader in = null;
+        String response = "";
+        String protocol = "https";
+        try {
+            URL url = new URL("http://api.urbandictionary.com/v0/define?term=" + word);
+            connection = (HttpURLConnection) url.openConnection();
+	    connection.setRequestProperty("User-Agent", "Goat IRC Bot v" +
+					  BotStats.getInstance().getVersion() +
+					  " (" + System.getProperty("os.name") +
+					  " v" + System.getProperty("os.version") + ';'
+					  + System.getProperty("os.arch") + ")");
+            connection.setConnectTimeout(3000);  // 3 seconds, no waiting around
+            connection.setReadTimeout(3000);  // 3 seconds, no waiting around
+            connection.connect();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                return new JSONObject("{\"error\":\"urban dictionary gave me a " + connection.getResponseCode() + " :(\"}");
+            }
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine = "" ;
+            while ((inputLine = in.readLine()) != null)
+                response += inputLine;
+
+        } catch  (SocketTimeoutException e) {
+            response = "{\"error\":\"I got bored waiting for UrbanDictionary.\"}" ;
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = "{\"error\":\"I had an I/O problem when trying to talk to UrbanDictionary :(\"}";
+        } finally {
+            if(connection!=null) connection.disconnect();
+            try {
+                if(in!=null) in.close();
+            } catch (IOException ioe) {
+                System.err.println("UrbanDictionary: Could not close input stream!");
+                ioe.printStackTrace();
+            }
+        }
+        return new JSONObject(response);
+    }
+}
